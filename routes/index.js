eef18e5154500536fc2f090398cbc8cd7fddbc10
Jordan kimball <jordan.matthew.kimball@gmail.com>
2018-08-14 14:22:52 -0400

    Started process of using Postgresql database instead of Sqlite3 database

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,27 @@ var router = express.Router();
 var main_controller = require('../controllers/mainController');
 
 
+//Added code to connect to this Heroku database I created in PostgreSQL
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM AvPokemon');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+
 //MAIN PAGES
 
 /* GET home page. */
