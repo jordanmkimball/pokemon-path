c83f5d36a236a5cee5c77266d4605139ec8b9549
Jordan Kimball <jordan.matthew.kimball@gmail.com>
2019-01-28 21:31:36 -0500

    Added ability to select which language the localizedString is translated in

--- views/clientSide-js/myAccountScripts.js
+++ views/clientSide-js/myAccountScripts.js
@@ -6,14 +6,31 @@ const clearButton = document.querySelector('button');
 let welcomeUserBlock = document.getElementById('welcomeUserBlock');
 let welcomeUserText = document.getElementById('welcomeUserText');
 let dateAndTime = document.getElementById('dateAndTime');
-let localizedString = document.getElementById('localizedString')
+let localizedString = document.getElementById('localizedString');
+let browserLangText = document.getElementById('browserLangText');
+let notTranslatedWarning = document.getElementById('notTranslatedWarning');
+let browserLangBlock = document.getElementById('browserLangBlock');
+let userLangText = document.getElementById('userLangText');
+
+//variables for the language selection radio form
+let browserBox = document.getElementById('browser');
+let englishBox = document.getElementById('english');
+let spanishBox = document.getElementById('spanish');
+let frenchBox = document.getElementById('french');
+let germanBox = document.getElementById('german');
+let chineseBox = document.getElementById('chinese');
+let hindiBox = document.getElementById('hindi');
+let arabicBox = document.getElementById('arabic');
+let malayBox = document.getElementById('malay');
+let russianBox = document.getElementById('russian');
+
 
 
 //if firstName already exists with a value in local storage, userName is equal to the localStorage value. Otherwise userName is empty
 let userName = localStorage.getItem("firstName") ? localStorage.getItem("firstName") : "";
 console.log(userName);
-//if lang already exists with a value in sessionStorage, lang is equal to sessionStorage value. Otherwise lang is empty
-let langPreference = sessionStorage.getItem("lang") ? sessionStorage.getItem("lang") : "";
+//if langPref already exists with a value in localStorage userLangPref is equal to the localStorage value. Otherwise langPref is empty
+let userLangPref = localStorage.getItem("langPref") ? localStorage.getItem("langPref") : "";
 
 
 //Converts a string into titleCase
@@ -42,9 +59,12 @@ if(userName != ""){
     switchVisability();
 }
 
-//Function that handles taking the users first name text input and moving it to localStorage.
+
+
+//Function that handles taking users my account form selections and recording them in localStorage
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    console.log(input.value);
     userName = input.value;
     //Function won't accept empty strings or a name over 70 characters long
     if(userName == "" || userName.length > 70){
@@ -59,14 +79,55 @@ form.addEventListener('submit', function(e){
         localStorage.setItem("firstName", userName);
         //Make the nameForm hidden, and the welcomeUserText visiable
         switchVisability();
-    }
+
+        //EVALUATING THE CHOOSE LANGUAGE RADIO BUTTON Add the value of the selected button to localStorage. 
+        if(browserBox.checked){
+            userLangPref = browserBox.value;
+        }
+        else if(englishBox.checked){
+            userLangPref = englishBox.value;
+        }
+        else if(spanishBox.checked){
+            userLangPref = spanishBox.value;
+        }
+        else if(frenchBox.checked){
+            userLangPref = frenchBox.value;
+        }
+        else if(germanBox.checked){
+            userLangPref = germanBox.value;
+        }
+        else if(chineseBox.checked){
+            userLangPref = chineseBox.value;
+        }
+        else if(hindiBox.checked){
+            userLangPref = hindiBox.value;
+        }
+        else if(arabicBox.checked){
+            userLangPref = arabicBox.value;
+        }
+        else if(malayBox.checked){
+            userLangPref = malayBox.value;
+        }
+        else if(russianBox.checked){
+            userLangPref = russianBox.value;
+        }
+        localStorage.setItem("langPref", userLangPref);
+        setlocalizedString();
+        console.log("userLangPref: " + userLangPref);
+        console.log("localStorage: " + JSON.stringify(localStorage));
+    } 
 });
+
+
 
 //Clears user's name and resets local storage. 
 clearButton.addEventListener('click', function() {
     //Clear local Storage
     localStorage.clear();
+    userLangPref = "";
+    console.log("userLangPref: " + userLangPref);
     switchVisability();
+    setlocalizedString();
     //We reset the welcomeUserText to the default of user
     welcomeUserText.textContent = welcomeUserText.textContent.replace(userName, "user");
 });
@@ -87,7 +148,6 @@ var getFirstBrowserLanguage = function () {
     browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
     i,
     language;
-
     // support for HTML 5.1 "navigator.languages"
     if (Array.isArray(nav.languages)) {
       for (i = 0; i < nav.languages.length; i++) {
@@ -97,7 +157,6 @@ var getFirstBrowserLanguage = function () {
         }
       }
     }
-
     // support for other well known properties in browsers
     for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
       language = nav[browserLanguagePropertyKeys[i]];
@@ -105,57 +164,113 @@ var getFirstBrowserLanguage = function () {
         return language;
       }
     }
-
     return null;
   };
 
 
-console.log(getFirstBrowserLanguage());
-
-//Storing users local language setting in session Storage
-if(langPreference == ""){
-    langPreference = getFirstBrowserLanguage();
-    sessionStorage.setItem("lang", langPreference);
+//Get the browsers preferred language
+var browserLangPref = getFirstBrowserLanguage();
+//If the user hasn't selected a language preference, the browser language preference will be used as default. 
+if(userLangPref == ""){
+    userLangPref = browserLangPref;
 }
 
-console.log(sessionStorage.getItem("lang"));
+console.log("browserLangPref: " + browserLangPref);
+console.log("userLangPref: " + userLangPref);
 
-//Localizing the "localizedString" based on the user's language settings
+
+
+//changes "localizedString" based on the user's language settings
 //Spanish
-if(langPreference.startsWith('es')){
-    localizedString.textContent = "Esta oración cambia según la configuración de idioma de su navegador";
+function setlocalizedString() {
+    if(userLangPref.startsWith('es')){
+        localizedString.textContent = "\"Esta oración cambia según la configuración de idioma de su navegador\"";
+    }
+    //French
+    else if(userLangPref.startsWith('fr')){
+        localizedString.textContent = "\"Cette phrase change en fonction des paramètres de langue de votre navigateur\"";
+    }
+    //German
+    else if(userLangPref.startsWith('de')){
+        localizedString.textContent = "\"Dieser Satz ändert sich je nach Spracheinstellung Ihres Browsers\"";
+    }
+    //Chinese
+    else if(userLangPref.startsWith('zh')){
+        localizedString.textContent = "这句话根据浏览器的语言设置而变化";
+    }
+    //Hindi
+    else if(userLangPref.startsWith('hi')){
+        localizedString.textContent = "यह वाक्य आपके ब्राउज़र की भाषा सेटिंग के आधार पर बदलता है।";
+    }
+    //Arabic
+    else if(userLangPref.startsWith("ar")){
+        localizedString.textContent = "يتغير هذا النص بناءً على إعداد لغة متصفحك";
+    }
+    //Malay
+    else if(userLangPref.startsWith("ms")){
+        localizedString.textContent = "\"Perubahan ayat ini berdasarkan tetapan bahasa penyemak imbas anda\"";
+    }
+    //Russian
+    else if(userLangPref.startsWith("ru")){
+        localizedString.textContent = "Это предложение изменяется в зависимости от языковой настройки вашего браузера";
+    }
+    //English
+    else{
+        localizedString.textContent = "\"This text changes based on your browser language setting\"";
+    }
+}
+
+//Translates the localizedString into the users preferred language
+setlocalizedString();
+
+
+
+//Displaying the language currently used by the user's browser
+//Spanish
+if(browserLangPref.startsWith('es')){
+    browserLangText.textContent = "Spanish";
 }
 //French
-else if(langPreference.startsWith('fr')){
-    localizedString.textContent = "Cette phrase change en fonction des paramètres de langue de votre navigateur";
+else if(browserLangPref.startsWith('fr')){
+    browserLangText.textContent = "French";
 }
 //German
-else if(langPreference.startsWith('de')){
-    localizedString.textContent = "Dieser Satz ändert sich je nach Spracheinstellung Ihres Browsers";
+else if(browserLangPref.startsWith('de')){
+    browserLangText.textContent = "German";
 }
 //Chinese
-else if(langPreference.startsWith('zh')){
-    localizedString.textContent = "这句话根据浏览器的语言设置而变化";
+else if(browserLangPref.startsWith('zh')){
+    browserLangText.textContent = "Chinese";
 }
 //Hindi
-else if(langPreference.startsWith('hi')){
-    localizedString.textContent = "यह वाक्य आपके ब्राउज़र की भाषा सेटिंग के आधार पर बदलता है।";
+else if(browserLangPref.startsWith('hi')){
+    browserLangText.textContent = "Hindi";
 }
 //Arabic
-else if(langPreference.startsWith("ar")){
-    localizedString.textContent = "تتغير هذه الجملة بناءً على إعداد لغة متصفحك";
+else if(browserLangPref.startsWith("ar")){
+    browserLangText.textContent = "Arabic";
 }
 //Malay
-else if(langPreference.startsWith("ms")){
-    localizedString.textContent = "Perubahan ayat ini berdasarkan tetapan bahasa penyemak imbas anda";
+else if(browserLangPref.startsWith("ms")){
+    browserLangText.textContent = "Malay";
 }
 //Russian
-else if(langPreference.startsWith("ru")){
-    localizedString.textContent = "Это предложение изменяется в зависимости от языковой настройки вашего браузера"
+else if(browserLangPref.startsWith("ru")){
+    browserLangText.textContent = "Russian";
 }
 //English
-else{
-    localizedString.textContent = "This text changes based on your browser language setting";
+else if(browserLangPref.startsWith("en")){
+    browserLangText.textContent = "English";
 }
-
-
+//Display warning if the browsers language preference could not be retrieved
+else if(browserLangPref == null){
+    browserLangBlock.textContent = "Error: could not retrieve user’s browser language preference. English is being displayed instead."
+}
+else{
+    browserLangText.textContent = "ISO 639-2 Language Code: " + browserLangPref;
+    //If langPref exists in local storage return false otherwise return true. 
+    if((localStorage.getItem("langPref") ? false : true) || localStorage.getItem("langPref") == "browser"){
+        //Add a warning that language won't be translated, as it is not one of the supported languages. 
+        notTranslatedWarning.textContent = "This language is not currently one of the 9 languages supported for the above text. English is being displayed instead."
+    }
+}
